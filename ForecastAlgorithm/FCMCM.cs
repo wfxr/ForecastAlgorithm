386ce64e8e4e05d2eb89007199434323cc44b165
Wenxuan Zhang <wenxuan-zhang@outlook.com>
2015-09-20 12:30:06 +0800

    重构

--- ForecastAlgorithm/FCMCM.cs
+++ ForecastAlgorithm/FCMCM.cs
@@ -1,6 +1,5 @@
 ﻿using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
-using MathNet.Numerics.Statistics;
 using Wfxr.DataAnalysis.Clustering;
 using Wfxr.DataAnalysis.MarkovModel;
 using Wfxr.Utility.ContainerExtension;
@@ -13,23 +12,23 @@ namespace ForecastAlgorithm
     public class FcmMc {
         private int[] _obsIdx;
         private readonly Fcm _fcm;
-        private  ClusterReport _fcmResult;
         private readonly MarkovChain _mc;
         private int _clusterCount;
+        private ClusterReport _fcmResult;
         private int _transStep;
-        public Matrix<double> Center => _fcmResult?.Center;
+        private Matrix<double> Center => _fcmResult?.Center;
 
         public FcmMc(double[,] obs) {
             _fcm = new Fcm(obs);
             _mc = new MarkovChain();
         }
-
         /// <summary>
         ///     模型训练
         /// </summary>
         /// <param name="clusterCount">分类数目</param>
         /// <param name="transStep">转移步数</param>
-        public void Train(int clusterCount, int transStep) {
+        public void Train(int clusterCount, int transStep)
+        {
             _clusterCount = clusterCount;
             _transStep = transStep;
             _fcmResult = _fcm.Run(_clusterCount);
@@ -37,21 +36,23 @@ namespace ForecastAlgorithm
             _mc.Train(_obsIdx, _transStep);
         }
 
+        /// <summary>
+        ///     由最近的已知状态预测下一个最可能状态
+        /// </summary>
+        /// <returns></returns>
         public double[] Forecast() {
-            // 由最后两个已知状态预测下一个最可能状态
             var obs = _obsIdx.Sub(_obsIdx.Length - _transStep, _transStep);
             var forecastIdx = _mc.Forecast(obs).Key;
             return _fcmResult.Center.Row(forecastIdx).ToArray();
         }
 
-        public double[] WeightedForecast() {
+        public double[] WeightedForecast()
+        {
             var obs = _obsIdx.Sub(_obsIdx.Length - _transStep, _transStep);
             var probs = _mc.T.Row(obs);
             var result = DenseVector.Create(Center.ColumnCount, 0.0);
-            for (var i = 0; i < Center.RowCount; ++i) {
-                var tmp = Center.Row(i).Multiply(probs[i]);
+            for (var i = 0; i < Center.RowCount; ++i) 
                 result.Add(Center.Row(i).Multiply(probs[i]), result);
-            }
 
             return result.ToArray();
         }
